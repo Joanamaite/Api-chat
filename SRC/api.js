f836710c0b86daec79918efef437e7efb64f6846
Joana <joanamaite0@gmail.com>
2023-05-27 01:55:13 -0300

    ultima parte

--- SRC/api.js
+++ SRC/api.js
@@ -2,10 +2,78 @@ var express = require("express");
 var app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
+const token = require("./util/token.js");
+const salaController = require("./controllers/salaController");
+const usuarioController = require("./controllers/usuarioController");
 const router = express.Router();
-app.use('/', router.get('/',(req,resp)=>{
+
+app.use('/', router.get('/',(req,res)=>{
     res.status(200).send("<h1>API-CHAT<h1>")
+}));
+
+app.use("/sobre",router.get("/sobre", (req, res, next)=>{
+    res.status(200).send({
+        "nome":"API - CHAT",
+        "versão":"0.1.0",
+        "autor":"Joana"
+    })
+}));
+app.use("/sala/criar", router.post("/sala/criar", async (req, res) => {
+    if (!token.checkToken(req.headers.token, req.body.iduser, req.body.nick)) {
+      return res.status(400).send({ msg: "Usuário não autorizado" });
+    }
+  
+    // Extrair os dados da requisição
+    const { nome, tipo } = req.body;
+  
+    // Chamar o controller para criar a sala
+    const sala = await salaController.criarSala(nome, tipo);
+  
+    if (sala) {
+      return res.status(200).send({ msg: "Sala criada com sucesso", sala });
+    } else {
+      return res.status(400).send({ msg: "Erro ao criar sala" });
+    }
+  }));
+//Listar Salas
+app.use("/salas", router.get("/salas", async (req, res, next) => {
+  if (await token.checkToken(req.body.token, req.body.iduser, req.body.nick)) {
+    let resp = await salaController.get();
+    res.status(200).send(resp);
+    
+  } else {
+    res.status(400).send({ msg: "Usuário não autorizado" });
+  }
+}));
+
+app.use("/entrar", router.post("/entrar", async(req, res, next)=>{
+    const usuarioController = require("./controllers/usuarioController");
+    let resp = await usuarioController.entrar(req.body.nick);
+    res.status(200).send(resp);
+}));
+
+app.use("/sala/entrar", router.put("/sala/entrar", async (req, res)=>{
+    let resp = await salaController.entrar(req.body.iduser, req.body.idsala);
+    res.status(200).send(resp);
+}));
+
+//enviar mensagem
+app.use("/sala/mensagem/", router.post("/sala/mensagem", async (req, res) => {
+    if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+    let resp= await salaController.enviarMensagem(req.headers.nick, req.body.msg,req.body.idsala);
+    res.status(200).send(resp);
+  }))
+  //listar mensagem
+  app.use("/sala/mensagens/", router.get("/sala/mensagens", async (req, res) => {
+    if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
+    let resp= await salaController.buscarMensagens(req.query.idSala, req.query.timestamp);
+    res.status(200).send(resp);
+  }))
+  app.use("/sala/sair/", router.put("/sala/sair", async (req, res) => {
+	if(!token.checkToken(req.headers.token,req.body.iduser,req.body.nick)) return false;
+	let resp= await salaController.sairSala(req.query.idsala, req.body.iduser);
+	res.status(200).send(resp);
 }))
+  
 
 module.exports=app;
---
+++ SRC/controllers/salaController.js
@@ -0,0 +1,73 @@
+const salaModel = require('../models/salaModel');
+const usuarioModel = require('../models/usuarioModel');
+
+exports.get = async (req, res) => {
+  return await salaModel.listarSalas();
+};
+
+exports.entrar = async (iduser, idsala) => {
+  const sala = await salaModel.buscarSala(idsala);
+
+  let user = await usuarioModel.buscarUsuario(iduser);
+  user.sala = {_id:sala._id, nome:sala.nome, tipo:sala.tipo};
+  if (await usuarioModel.alterarUsuarios(user)) {
+    return { msg: "OK", timestamp: Date.now() };
+  }
+  return false;
+};
+
+exports.enviarMensagem = async (nick, msg, idsala) => {
+  const sala = await salaModel.buscarSala(idsala);
+  if (!sala.msgs) {
+    sala.msgs = [];
+  }
+  const timestamp = Date.now();
+  sala.msgs.push({
+    timestamp: timestamp,
+    msg: msg,
+    nick: nick
+  });
+  let resp = await salaModel.atualizarMensagens(sala);
+  return { msg: "OK", timestamp: timestamp };
+};
+
+exports.buscarMensagens = async (idsala, timestamp) => {
+  let mensagens = await salaModel.buscarMensagens(idsala, timestamp);
+  return {
+    timestamp: mensagens[mensagens.length - 1].timestamp,
+    msgs: mensagens
+  };
+};
+
+exports.sairSala = async (idsala, iduser) => {
+  let user = await usuarioModel.buscarUsuario(iduser);
+  let resp = await exports.enviarMensagem(iduser, "Sai da sala!", idsala); 
+  delete resp.sala;
+  console.log(user);
+  if (await usuarioModel.alterarUsuarios(user)) {
+    user = await usuarioModel.buscarUsuario(iduser);
+    console.log(user);
+    return { msg: "OK", timestamp: Date.now() };
+  }
+
+  return false;
+};
+
+exports.criarSala = async (nome, tipo) => {
+  const sala = {
+    nome: nome,
+    tipo: tipo,
+    msgs: []
+  };
+
+  const novaSala = await salaModel.criarSala(sala);
+  return novaSala;
+};
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
